1e634219e6ffded21e32ac7f33a9c20d45b78fc5
asjerab <asjer.contact@gmail.com>
2024-05-07 11:58:21 +0200

    La til reverse crypetering for login

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+import bcrypt
 import mysql.connector
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 # Database configuration
-host = "10.2.1.218"
-user = "asjer"
+host = "10.100.1.101"
+user = "asjer" 
 password = "myPass"
 database = "weatherapp"
 
@@ -40,6 +41,10 @@ def signUp():
 def FAQ():
     return render_template('FAQ.html')
 
+@app.route('/userLog')
+def userLog():
+    return render_template('userLog.html')
+
 @app.route('/application')
 def app_function():
     username = session.get('username')
@@ -60,9 +65,12 @@ def signup():
         try:
             cursor = db.cursor()
 
+            # Hash the password using bcrypt
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
             # Insert user data into the database using placeholders
             insert_query = "INSERT INTO user (username, password) VALUES (%s, %s)"
-            user_data = (username, password)
+            user_data = (username, hashed_password)
             cursor.execute(insert_query, user_data)
 
             db.commit()
@@ -75,6 +83,7 @@ def signup():
             print("Something went wrong: {}".format(err))
             return render_template('signUp.html', msg='Registration failed')
 
+
 @app.route('/login', methods=['POST'])
 def log_in():
     if request.method == 'POST':
@@ -85,23 +94,30 @@ def log_in():
             cursor = db.cursor(buffered=True)
 
             # Select user data from the database using placeholders
-            select_query = "SELECT * FROM user WHERE username = %s AND password = %s"
-            user_data = (username, password)
+            select_query = "SELECT * FROM user WHERE username = %s"
+            user_data = (username,)
             cursor.execute(select_query, user_data)
             
             user = cursor.fetchone()  # Fetch the first row of the result
             cursor.close()
 
             if user:
-                session['username'] = username  # Store username in session
-                # Redirect to the application page after successful login
-                return redirect(url_for('app_function'))
+                hashed_password = user[2]  # Get the hashed password from the result
+                if bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')):
+                    session['username'] = username  # Store username in session
+                    # Redirect to the application page after successful login
+                    return redirect(url_for('app_function'))
+                else:
+                    return render_template('login.html', msg='Login failed')
             else:
                 return render_template('login.html', msg='Login failed')
 
         except mysql.connector.Error as err:
             print("Something went wrong: {}".format(err))
             return render_template('login.html', msg='Login failed')
+                
+        
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
